ab0520b35c2e43d7d338a255cfa165458a342228
mochenya <mochen20070702@gmail.com>
2025-09-01 18:14:18 +0800

    SAD

--- ksu/asus.sh
+++ ksu/asus.sh
@@ -31,6 +31,7 @@ if [[ -d "$suspatchesdir" ]]; then
     git am "$patch_file"
   done
   echo "CONFIG_KSU=y" >> "${defconfig_file}"
+  echo "CONFIG_KSU_SUSFS_SUS_SU=n" >> "${defconfig_file}"
   echo "CONFIG_KSU_MANUAL_HOOK=y" >> "${defconfig_file}"
   echo "CONFIG_KSU_SUSFS=y" >> "${defconfig_file}"
   echo "CONFIG_KSU_SUSFS_HAS_MAGIC_MOUNT=y" >> "${defconfig_file}"
