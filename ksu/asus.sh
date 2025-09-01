064c31ed3823525204ce9102f9d78d86ad90a188
mochenya <mochen20070702@gmail.com>
2025-09-01 14:29:51 +0800

    build: change Sukisu

--- ksu/asus.sh
+++ ksu/asus.sh
@@ -6,36 +6,46 @@ export maindir="$(pwd)"
 export outside="${maindir}/.."
 source "${outside}/$1env"
 
-curl -LSs "https://raw.githubusercontent.com/SukiSU-Ultra/SukiSU-Ultra/main/kernel/setup.sh" | bash -s susfs-main
-git add . && git commit -am "drivers: KernelSU"
+curl -LSs "https://raw.githubusercontent.com/SukiSU-Ultra/SukiSU-Ultra/main/kernel/setup.sh" | bash -s nongki
+git add . && git commit -am "drivers: SukiSU-Ultra"
 KSU_git_ver=$(cd KernelSU && git rev-list --count HEAD)
 KSU_ver=$(($KSU_git_ver + 10000 + 200))
 
-patchesdir="$outside/ksu/patches/$(echo $kernel_ver | cut -d. -f1,2)"
-suspatchesdir="$outside/ksu/sus_patches/$(echo $kernel_ver | cut -d. -f1,2)"
+# patchesdir="$outside/ksu/patches/$(echo $kernel_ver | cut -d. -f1,2)"
+# suspatchesdir="$outside/ksu/sus_patches/$(echo $kernel_ver | cut -d. -f1,2)"
+sukisupatchesdir="$outside/SukiSU/patches/$(echo $kernel_ver | cut -d. -f1,2)"
 
-if [[ -d "$patchesdir" ]]; then
-  for patch_file in "$patchesdir"/*.patch ; do
-    git am "$patch_file"
+# if [[ -d "$patchesdir" ]]; then
+#   for patch_file in "$patchesdir"/*.patch ; do
+#     git am "$patch_file"
+#   done
+# else
+#   echo "patching ksu failed, the kernel version you want to patch doesnt have patches here yet"
+#   exit 1
+# fi
+
+# if [[ -d "$suspatchesdir" ]]; then
+#   for patch_file in "$suspatchesdir"/*.patch ; do
+#     git am "$patch_file"
+#   done
+# else
+#   echo "patching ksu susfs failed, the kernel version you want to patch doesnt have patches here yet"
+#   exit 1
+# fi
+
+if [[ -d "$sukisupatchesdir" ]]; then
+  for patch_file in "$sukisupatchesdir"/*.patch ; do
+    patch -p1 < "$patch_file"
   done
 else
-  echo "patching ksu failed, the kernel version you want to patch doesnt have patches here yet"
+  echo "patching SukiSU-Ultra failed, the kernel version you want to patch doesnt have patches here yet"
   exit 1
 fi
 
-if [[ -d "$suspatchesdir" ]]; then
-  for patch_file in "$suspatchesdir"/*.patch ; do
-    git am "$patch_file"
-  done
-else
-  echo "patching ksu susfs failed, the kernel version you want to patch doesnt have patches here yet"
-  exit 1
-fi
-
-sed -i "s/\(CONFIG_LOCALVERSION=\)\(.*\)/\1\"-${kernel_name}-ks${KSU_ver}sus\"/" "${defconfig_file}"
+sed -i "s/\(CONFIG_LOCALVERSION=\)\(.*\)/\1\"-${kernel_name}-SukiSU-${KSU_ver}\"/" "${defconfig_file}"
 
 echo "$(grep 'CONFIG_LOCALVERSION=' ${defconfig_file})"
 
 echo -e " \nincludes SukiSU Ultra, ver ${KSU_ver}" >> banner_append
-echo -e " \nincludes SuSFS-main" >> banner_append
+# echo -e " \nincludes SuSFS-main" >> banner_append
 
