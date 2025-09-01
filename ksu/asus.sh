afa929ebbad7c60cf29ff81593c03356c77259e9
mochenya <mochen20070702@gmail.com>
2025-09-01 17:46:35 +0800

    mmm

--- ksu/asus.sh
+++ ksu/asus.sh
@@ -6,7 +6,9 @@ export maindir="$(pwd)"
 export outside="${maindir}/.."
 source "${outside}/$1env"
 
+# Import SukiSU-Ultra
 curl -LSs "https://raw.githubusercontent.com/SukiSU-Ultra/SukiSU-Ultra/main/kernel/setup.sh" | bash -s susfs-main
+echo "Applying: SukiSU-Ultra"
 git add . && git commit -am "drivers: SukiSU-Ultra"
 KSU_git_ver=$(cd KernelSU && git rev-list --count HEAD)
 KSU_ver=$(($KSU_git_ver + 10000 + 200))
@@ -18,6 +20,7 @@ if [[ -d "$patchesdir" ]]; then
   for patch_file in "$patchesdir"/*.patch ; do
     git am "$patch_file"
   done
+  echo "patching ksu succeeded."
 else
   echo "patching ksu failed, the kernel version you want to patch doesnt have patches here yet"
   exit 1
@@ -27,21 +30,22 @@ if [[ -d "$suspatchesdir" ]]; then
   for patch_file in "$suspatchesdir"/*.patch ; do
     git am "$patch_file"
   done
-  echo "CONFIG_KSU_SUSFS=y" >> “${defconfig_file}”
-  echo "CONFIG_KSU_SUSFS_HAS_MAGIC_MOUNT=y" >> “${defconfig_file}”
-  echo "CONFIG_KSU_SUSFS_SUS_PATH=y" >> “${defconfig_file}”
-  echo "CONFIG_KSU_SUSFS_SUS_MOUNT=y" >> “${defconfig_file}”
-  echo "CONFIG_KSU_SUSFS_AUTO_ADD_SUS_KSU_DEFAULT_MOUNT=y" >> “${defconfig_file}”
-  echo "CONFIG_KSU_SUSFS_AUTO_ADD_SUS_BIND_MOUNT=y" >> “${defconfig_file}”
-  echo "CONFIG_KSU_SUSFS_SUS_KSTAT=y" >> “${defconfig_file}”
-  echo "CONFIG_KSU_SUSFS_SUS_OVERLAYFS=n" >> “${defconfig_file}”
-  echo "CONFIG_KSU_SUSFS_TRY_UMOUNT=y" >> “${defconfig_file}”
-  echo "CONFIG_KSU_SUSFS_AUTO_ADD_TRY_UMOUNT_FOR_BIND_MOUNT=y" >> “${defconfig_file}”
-  echo "CONFIG_KSU_SUSFS_SPOOF_UNAME=y" >> “${defconfig_file}”
-  echo "CONFIG_KSU_SUSFS_ENABLE_LOG=y" >> “${defconfig_file}”
-  echo "CONFIG_KSU_SUSFS_HIDE_KSU_SUSFS_SYMBOLS=y" >> “${defconfig_file}”
-  echo "CONFIG_KSU_SUSFS_SPOOF_CMDLINE_OR_BOOTCONFIG=y" >> “${defconfig_file}”
-  echo "CONFIG_KSU_SUSFS_OPEN_REDIRECT=y" >> “${defconfig_file}”
+  echo "CONFIG_KSU_SUSFS=y" >> "${defconfig_file}"
+  echo "CONFIG_KSU_SUSFS_HAS_MAGIC_MOUNT=y" >> "${defconfig_file}"
+  echo "CONFIG_KSU_SUSFS_SUS_PATH=y" >> "${defconfig_file}"
+  echo "CONFIG_KSU_SUSFS_SUS_MOUNT=y" >> "${defconfig_file}"
+  echo "CONFIG_KSU_SUSFS_AUTO_ADD_SUS_KSU_DEFAULT_MOUNT=y" >> "${defconfig_file}"
+  echo "CONFIG_KSU_SUSFS_AUTO_ADD_SUS_BIND_MOUNT=y" >> "${defconfig_file}"
+  echo "CONFIG_KSU_SUSFS_SUS_KSTAT=y" >> "${defconfig_file}"
+  echo "CONFIG_KSU_SUSFS_SUS_OVERLAYFS=n" >> "${defconfig_file}"
+  echo "CONFIG_KSU_SUSFS_TRY_UMOUNT=y" >> "${defconfig_file}"
+  echo "CONFIG_KSU_SUSFS_AUTO_ADD_TRY_UMOUNT_FOR_BIND_MOUNT=y" >> "${defconfig_file}"
+  echo "CONFIG_KSU_SUSFS_SPOOF_UNAME=y" >> "${defconfig_file}"
+  echo "CONFIG_KSU_SUSFS_ENABLE_LOG=y" >> "${defconfig_file}"
+  echo "CONFIG_KSU_SUSFS_HIDE_KSU_SUSFS_SYMBOLS=y" >> "${defconfig_file}"
+  echo "CONFIG_KSU_SUSFS_SPOOF_CMDLINE_OR_BOOTCONFIG=y" >> "${defconfig_file}"
+  echo "CONFIG_KSU_SUSFS_OPEN_REDIRECT=y" >> "${defconfig_file}"
+  echo "patching ksu susfs succeeded."
 else
   echo "patching ksu susfs failed, the kernel version you want to patch doesnt have patches here yet"
   exit 1
