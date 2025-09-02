a4afc731ff43f97203233a185d17b871fe403c69
mochenya <mochen20070702@gmail.com>
2025-09-02 11:09:53 +0800

    sasa

--- ksu/asus.sh
+++ ksu/asus.sh
@@ -7,7 +7,7 @@ export outside="${maindir}/.."
 source "${outside}/$1env"
 
 # Import SukiSU-Ultra
-curl -LSs "https://raw.githubusercontent.com/SukiSU-Ultra/SukiSU-Ultra/main/kernel/setup.sh" | bash -s nongki
+curl -LSs "https://raw.githubusercontent.com/rsuntk/KernelSU/main/kernel/setup.sh" | bash -s main
 echo "Applying: SukiSU-Ultra"
 git add . && git commit -am "drivers: SukiSU-Ultra"
 KSU_git_ver=$(cd KernelSU && git rev-list --count HEAD)
