d3a0f933f34f90b3f73815af72b18337ee891fe1
mochenya <mochen20070702@gmail.com>
2025-09-02 10:34:00 +0800

    xxa

--- ksu/asus.sh
+++ ksu/asus.sh
@@ -7,7 +7,7 @@ export outside="${maindir}/.."
 source "${outside}/$1env"
 
 # Import SukiSU-Ultra
-curl -LSs "https://raw.githubusercontent.com/SukiSU-Ultra/SukiSU-Ultra/main/kernel/setup.sh" | bash -s main
+curl -LSs "https://raw.githubusercontent.com/SukiSU-Ultra/SukiSU-Ultra/main/kernel/setup.sh" | bash -s nongki
 echo "Applying: SukiSU-Ultra"
 git add . && git commit -am "drivers: SukiSU-Ultra"
 KSU_git_ver=$(cd KernelSU && git rev-list --count HEAD)
@@ -19,9 +19,9 @@ suspatchesdir="$outside/ksu/sus_patches/$(echo $kernel_ver | cut -d. -f1,2)"
 if [[ -d "$patchesdir" ]]; then
   for patch_file in "$patchesdir"/*.patch ; do
     git am "$patch_file"
-    # echo "patch config: ${defconfig_file}"
-    # echo "CONFIG_KSU=y" >> "${defconfig_file}"
-    # echo "CONFIG_KSU_MANUAL_HOOK=y" >> "${defconfig_file}"
+    echo "patch config: ${defconfig_file}"
+    echo "CONFIG_KSU=y" >> "${defconfig_file}"
+    echo "CONFIG_KSU_MANUAL_HOOK=y" >> "${defconfig_file}"
   done
   echo "patching ksu succeeded."
 else
@@ -29,32 +29,32 @@ else
   exit 1
 fi
 
-# if [[ -d "$suspatchesdir" ]]; then
-#   for patch_file in "$suspatchesdir"/*.patch ; do
-#     git am "$patch_file"
-#   done
-#   echo "CONFIG_KSU_SUSFS_SUS_SU is not set" >> "${defconfig_file}"
-#   echo "CONFIG_KSU_SUSFS=y" >> "${defconfig_file}"
-#   echo "CONFIG_KSU_SUSFS_HAS_MAGIC_MOUNT=y" >> "${defconfig_file}"
-#   echo "CONFIG_KSU_SUSFS_SUS_PATH=y" >> "${defconfig_file}"
-#   echo "CONFIG_KSU_SUSFS_SUS_MOUNT=y" >> "${defconfig_file}"
-#   echo "CONFIG_KSU_SUSFS_AUTO_ADD_SUS_KSU_DEFAULT_MOUNT=y" >> "${defconfig_file}"
-#   echo "CONFIG_KSU_SUSFS_AUTO_ADD_SUS_BIND_MOUNT=y" >> "${defconfig_file}"
-#   echo "CONFIG_KSU_SUSFS_SUS_KSTAT=y" >> "${defconfig_file}"
-#   echo "CONFIG_KSU_SUSFS_SUS_OVERLAYFS=n" >> "${defconfig_file}"
-#   echo "CONFIG_KSU_SUSFS_TRY_UMOUNT=y" >> "${defconfig_file}"
-#   echo "CONFIG_KSU_SUSFS_AUTO_ADD_TRY_UMOUNT_FOR_BIND_MOUNT=y" >> "${defconfig_file}"
-#   echo "CONFIG_KSU_SUSFS_SPOOF_UNAME=y" >> "${defconfig_file}"
-#   echo "CONFIG_KSU_SUSFS_ENABLE_LOG=y" >> "${defconfig_file}"
-#   echo "CONFIG_KSU_SUSFS_HIDE_KSU_SUSFS_SYMBOLS=y" >> "${defconfig_file}"
-#   echo "CONFIG_KSU_SUSFS_SPOOF_CMDLINE_OR_BOOTCONFIG=y" >> "${defconfig_file}"
-#   echo "CONFIG_KSU_SUSFS_OPEN_REDIRECT=y" >> "${defconfig_file}"
-#   cp "${defconfig_file}" defconfig.zip
-#   echo "patching ksu susfs succeeded."
-# else
-#   echo "patching ksu susfs failed, the kernel version you want to patch doesnt have patches here yet"
-#   exit 1
-# fi
+if [[ -d "$suspatchesdir" ]]; then
+  for patch_file in "$suspatchesdir"/*.patch ; do
+    git am "$patch_file"
+  done
+  echo "CONFIG_KSU_SUSFS_SUS_SU is not set" >> "${defconfig_file}"
+  echo "CONFIG_KSU_SUSFS=y" >> "${defconfig_file}"
+  echo "CONFIG_KSU_SUSFS_HAS_MAGIC_MOUNT=y" >> "${defconfig_file}"
+  echo "CONFIG_KSU_SUSFS_SUS_PATH=y" >> "${defconfig_file}"
+  echo "CONFIG_KSU_SUSFS_SUS_MOUNT=y" >> "${defconfig_file}"
+  echo "CONFIG_KSU_SUSFS_AUTO_ADD_SUS_KSU_DEFAULT_MOUNT=y" >> "${defconfig_file}"
+  echo "CONFIG_KSU_SUSFS_AUTO_ADD_SUS_BIND_MOUNT=y" >> "${defconfig_file}"
+  echo "CONFIG_KSU_SUSFS_SUS_KSTAT=y" >> "${defconfig_file}"
+  echo "CONFIG_KSU_SUSFS_SUS_OVERLAYFS=n" >> "${defconfig_file}"
+  echo "CONFIG_KSU_SUSFS_TRY_UMOUNT=y" >> "${defconfig_file}"
+  echo "CONFIG_KSU_SUSFS_AUTO_ADD_TRY_UMOUNT_FOR_BIND_MOUNT=y" >> "${defconfig_file}"
+  echo "CONFIG_KSU_SUSFS_SPOOF_UNAME=y" >> "${defconfig_file}"
+  echo "CONFIG_KSU_SUSFS_ENABLE_LOG=y" >> "${defconfig_file}"
+  echo "CONFIG_KSU_SUSFS_HIDE_KSU_SUSFS_SYMBOLS=y" >> "${defconfig_file}"
+  echo "CONFIG_KSU_SUSFS_SPOOF_CMDLINE_OR_BOOTCONFIG=y" >> "${defconfig_file}"
+  echo "CONFIG_KSU_SUSFS_OPEN_REDIRECT=y" >> "${defconfig_file}"
+  cp "${defconfig_file}" defconfig.zip
+  echo "patching ksu susfs succeeded."
+else
+  echo "patching ksu susfs failed, the kernel version you want to patch doesnt have patches here yet"
+  exit 1
+fi
 
 sed -i "s/\(CONFIG_LOCALVERSION=\)\(.*\)/\1\"-${kernel_name}-SukiSU-${KSU_ver}\"/" "${defconfig_file}"
 
